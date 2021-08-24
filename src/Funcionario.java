2c2c5210a704dd17b316818f1fd71705d7ad5224
Jonathas Andrade <Jonathas.andrade5@gmail.com>
2021-08-24 18:54:27 -0300

    fix: ajuste no método tostring

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -75,14 +75,14 @@ public class Funcionario {
 
   @Override
   public String toString() {
-    return "Funcionario {" +
-        "nome: '" + nome + '\'' +
-        "sobrenome: '" + sobrenome + '\'' +
-        "endereco: " + endereco.toString() +
-        "cpf: '" + cpf + '\'' +
-        "idade: " + idade +
-        "sexo: '" + sexo + '\'' +
-        "telefone: '" + telefone + '\'' +
+    return "Funcionario {\n" +
+        "nome: '" + nome + '\'' + "\n" +
+        "sobrenome: '" + sobrenome + '\'' + "\n" +
+        "endereco: " + endereco.toString() + "\n" +
+        "cpf: '" + cpf + '\'' + "\n" +
+        "idade: " + idade + "\n" +
+        "sexo: '" + sexo + '\'' + "\n" +
+        "telefone: '" + telefone + '\'' + "\n" +
       '}';
   }
 }
